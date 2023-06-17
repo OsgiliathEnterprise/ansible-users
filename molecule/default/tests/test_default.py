65f8f15aa187dd19c6b4ecc60e956cc9ddad27e0
Charlie Mordant <cmordant1@gmail.com>
2023-06-17 18:26:05 +0200

    add email support for users

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,6 +10,15 @@ def test_user_exists(host):
     assert '1' in cmd.stdout
 
 
+def test_user_mail_exists(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipa user-find cmordant | \
+    grep -c ' Email address: cmordant@osgiliath.test'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
 def test_groups_cmordante_users(host):
     command = """set -o pipefail && echo '123ADMin'| \
     kinit admin > /dev/null && \
